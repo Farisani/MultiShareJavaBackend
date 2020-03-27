7440a097fadf7cf6ca66c7ee5c222b507685045d
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2020-03-27 18:34:06 +0200

    Added user info service implementation

--- MultiShareJavaBackendBusiness/src/main/java/za/co/multishare/service/impl/UserInfoServiceImpl.java
+++ MultiShareJavaBackendBusiness/src/main/java/za/co/multishare/service/impl/UserInfoServiceImpl.java
@@ -1,9 +1,16 @@
 package za.co.multishare.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import za.co.multishare.domain.dto.UserInfoDto;
+import za.co.multishare.domain.entity.UserInfo;
 import za.co.multishare.repository.repository.UserInfoRepository;
 import za.co.multishare.service.UserInfoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
@@ -13,5 +20,35 @@ public class UserInfoServiceImpl implements UserInfoService {
     @Autowired
     public UserInfoServiceImpl(final UserInfoRepository userInfoRepository) {
         this.userInfoRepository = userInfoRepository;
+    }
+
+    @Override
+    public List<UserInfo> findAll(Integer pageNumber, Integer pageSize) {
+        final Pageable page = PageRequest.of(pageNumber, pageSize);
+        return  userInfoRepository.findAll(page).toList();
+    }
+
+    @Override
+    public List<UserInfo> createUserInfo(List<UserInfoDto> userInfoDtoList) {
+
+        final List<UserInfo> userInfoList = new ArrayList<>();
+
+        userInfoDtoList.forEach(userInfoDto ->
+            userInfoList.add(new UserInfo(null,
+                    userInfoDto.getRecordValidFromDate(),
+                    userInfoDto.getRecordValidToDate())));
+
+        return userInfoRepository.saveAll(userInfoList);
+    }
+
+    @Override
+    public void deleteUserInfo(List<Long> userInfoIdList) {
+        final List<UserInfo> userInfoList = userInfoRepository.findAllById(userInfoIdList);
+        userInfoRepository.deleteAll(userInfoList);
+    }
+
+    @Override
+    public List<UserInfo> updateUserInfo(List<UserInfo> userInfoList) {
+        return userInfoRepository.saveAll(userInfoList);
     }
 }
