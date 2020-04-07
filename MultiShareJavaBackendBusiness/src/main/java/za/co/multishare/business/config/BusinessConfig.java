28bfd33ba91383b55b81e95d47b3dbf8ac0d2313
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2020-04-07 22:03:50 +0200

    Added password hashing

--- MultiShareJavaBackendBusiness/src/main/java/za/co/multishare/business/config/BusinessConfig.java
+++ MultiShareJavaBackendBusiness/src/main/java/za/co/multishare/business/config/BusinessConfig.java
@@ -1,9 +1,17 @@
 package za.co.multishare.business.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan({"za.co.multishare.repository.config", "za.co.multishare.business.service"})
 public class BusinessConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
