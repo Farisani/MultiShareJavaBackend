3fe67c85a61ee4b3c885d74ca792cd7c3ec64b8d
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2020-10-05 19:23:10 +0200

    Work in progress

--- src/main/java/za/co/multishare/endpoint/AuthenticationController.java
+++ src/main/java/za/co/multishare/endpoint/AuthenticationController.java
@@ -8,25 +8,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.multishare.domain.dto.LoginDto;
+import za.co.multishare.domain.entity.UserRoleInfo;
+import za.co.multishare.repository.UserRoleInfoRepository;
 import za.co.multishare.service.UserAuthenticationService;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/security/")
 public class AuthenticationController {
 
     private final UserAuthenticationService userAuthenticationService;
+    private final UserRoleInfoRepository userRoleInfoRepository;
 
     @Autowired
-    public AuthenticationController(final UserAuthenticationService userAuthenticationService) {
+    public AuthenticationController(final UserAuthenticationService userAuthenticationService,
+                                    final UserRoleInfoRepository userRoleInfoRepository) {
         this.userAuthenticationService = userAuthenticationService;
+        this.userRoleInfoRepository = userRoleInfoRepository;
     }
 
     @PostMapping("authenticate")
     public ResponseEntity<Map<String, String>> authenticate(@RequestBody final LoginDto loginDto) {
         final Map<String, String> authenticationResults = userAuthenticationService
                 .authenticateUser(loginDto.getUsername(), loginDto.getPassword());
+        final Long userInfoId = Long.valueOf(authenticationResults.get("userId"));
+        final List<UserRoleInfo> userRoleInfoList = userRoleInfoRepository.findByUserInfoUserInfoIdAndRecordValidToDateIsNull(userInfoId);
+        final List<String> rolesList = userRoleInfoList.stream().map(UserRoleInfo::getUserRole).collect(Collectors.toList());
+        authenticationResults.put("roles", rolesList.toString().substring(1, rolesList.toString().length() -1).replace(" ", ""));
        return new ResponseEntity<>(authenticationResults, HttpStatus.OK);
     }
 }
