47588ffad5e2f9e5187525d4eaf282d1da4cc7ef
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2020-10-06 17:46:56 +0200

    Work in progress

--- src/main/java/za/co/multishare/endpoint/UserRoleController.java
+++ src/main/java/za/co/multishare/endpoint/UserRoleController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.multishare.domain.entity.UserInfo;
 import za.co.multishare.domain.entity.UserRoleInfo;
+import za.co.multishare.repository.UserInfoRepository;
 import za.co.multishare.repository.UserRoleInfoRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +21,14 @@ import java.util.stream.Collectors;
 public class UserRoleController {
 
     private final UserRoleInfoRepository userRoleInfoRepository;
+    private final UserInfoRepository userInfoRepository;
 
     @Autowired
-    public UserRoleController(final UserRoleInfoRepository userRoleInfoRepository) { this.userRoleInfoRepository = userRoleInfoRepository; }
+    public UserRoleController(final UserRoleInfoRepository userRoleInfoRepository,
+                              final UserInfoRepository userInfoRepository) {
+        this.userRoleInfoRepository = userRoleInfoRepository;
+        this.userInfoRepository = userInfoRepository;
+    }
 
     @GetMapping("get/{userId}")
     public ResponseEntity<List<String>> getUserRoles(@PathVariable final Long userId) {
@@ -30,5 +38,19 @@ public class UserRoleController {
                 .stream()
                 .map(UserRoleInfo::getUserRole)
                 .collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+    @GetMapping("/makeAdmin/{userId}")
+    public ResponseEntity<Boolean> makeAdmin(@PathVariable final Long userId) {
+        final UserInfo userInfo = userInfoRepository.findById(userId).orElse(null);
+
+        final UserRoleInfo userRoleInfo = new UserRoleInfo(null,
+                "ADMIN_USER",
+                LocalDateTime.now(),
+                null, userInfo);
+
+        userRoleInfoRepository.saveAndFlush(userRoleInfo);
+
+        return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
     }
 }
